4255e99c23a73017b047d942f664488369d57de3
victorbillaud <victorbillaud@gmail.com>
2023-06-27 11:38:40 +0200

    updated solve functions and tests

--- src/main/scala/SudokuSolver.scala
+++ src/main/scala/SudokuSolver.scala
@@ -2,40 +2,68 @@ package sudoku
 
 object SudokuSolver {
 
-  type Cell = Option[Int]
-  type Grid = List[List[Cell]]
+  type Board = Array[Array[Int]]
 
-  def isValid(grid: Grid, row: Int, col: Int, num: Int): Boolean = {
-    // Check if `num` is not in already present in current row / current column / current 3x3 box
-    !((0 until 9).exists(i => grid(row)(i) == Some(num)) ||
-      (0 until 9).exists(i => grid(i)(col) == Some(num)) ||
-      (0 until 3).exists(i =>
-        (0 until 3).exists(j =>
-          grid(i + row - row % 3)(j + col - col % 3) == Some(num)
-        )
-      ))
+  def isValid(grid: Board, x: Int, y: Int, value: Int): Boolean = {
+    // check if the value is already in the row 
+    val row = grid(y)
+    val rowIsValid = !row.contains(value)
+
+    // check if the value is already in the column
+    val column = grid.map(r => r.apply(x))
+    val columnIsValid = !column.contains(value)
+
+    val boxX = x / 3
+    val boxY = y / 3
+    val box = for {
+      yb <- (boxY * 3) until (boxY * 3 + 3) // indices for rows in THIS box
+      xb <- (boxX * 3) until (boxX * 3 + 3) // same for cols
+    } yield grid(yb)(xb)
+    val bosIsValid = !box.contains(value)
+
+    rowIsValid && columnIsValid && bosIsValid
   }
 
-  def solve(grid: Grid): Option[Grid] = {
-    // Find an empty cell
-    val emptyCellOption = (for {
-      row <- grid.indices
-      col <- grid(row).indices
-      if grid(row)(col).isEmpty
-    } yield (row, col)).headOption
-
-    emptyCellOption match {
-      case Some((row, col)) =>
-        // Try numbers from 1 to 9 for the empty cell
-        (1 to 9).collectFirst {
-          case num if isValid(grid, row, col, num) =>
-            // If `num` can be placed in the current cell, call `solve` recursively
-            solve(grid.updated(row, grid(row).updated(col, Some(num))))
-        }.flatten
-
-      case None =>
-        // If there are no empty cells, the Sudoku puzzle has been solved
-        Some(grid)
-    }
+  def solve(sudoku: Board, x: Int = 0, y: Int = 0): Unit = {
+    if (y >= 9) println(prettyString(sudoku)) // print the solution
+    else if (x >= 9) solve(sudoku, 0, y + 1) // call solve for the next row
+    else if (sudoku(y)(x) > 0)
+      solve(
+        sudoku,
+        x + 1,
+        y
+      ) // solve for the next cell if the current one is already filled
+    else
+      (1 to 9).filter(value => isValid(sudoku, x, y, value)).foreach { value =>
+        // fill the sudoku board with the value
+        sudoku(y)(x) = value
+        // try the next cell
+        solve(sudoku, x + 1, y)
+        // remove the value
+        sudoku(y)(x) = 0
+      }
   }
+
+  def prettyString(sudoku: Board): String = {
+    sudoku
+      .grouped(3)
+      .map { bigGroup =>
+        bigGroup
+          .map { row =>
+            row
+              .grouped(3)
+              .map { smallGroup =>
+                smallGroup.mkString(" ", " ", " ")
+              }
+              .mkString("|", "|", "|")
+          }
+          .mkString("\n")
+      }
+      .mkString(
+        "+-------+-------+-------+\n",
+        "\n+-------+-------+-------+\n",
+        "\n+-------+-------+-------+"
+      )
+  }
+
 }
