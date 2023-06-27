7e1b127681001ba634e6c6bd818ba178c66e3fd0
Paul Guerre <pguerreftb@gmail.com>
2023-06-27 12:15:39 +0200

    Add basic function to extract and print the values of the JSON file as a two dimensional array of int

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,6 +9,9 @@ object Main extends ZIOAppDefault {
       _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem:")
       path <- Console.readLine
       _ <-  Console.printLine(s"You entered: $path")
+      jsonReader <- ZIO.succeed(new JSONReader)
+      array <- jsonReader.read(path)
+      _ <- Console.printLine(array.map(_.mkString(" ")).mkString("\n"))
       // Add your Sudoku solver logic here, utilizing ZIO and interacting with the ZIO Console
     } yield ()
 }
