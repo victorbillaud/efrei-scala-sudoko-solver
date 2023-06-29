d2e6240ddc813ec9dc7559647dbeb40981f7c571
victorbillaud <victorbillaud@gmail.com>
2023-06-29 23:30:19 +0200

    Change Main structure to handle errors

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,43 @@
 package sudoku
 
-import zio._
-
 import JSONReader._
+import zio._
 
 object Main extends ZIOAppDefault {
 
+  var defaultPath = "grids/grid.json"
+
   def run: ZIO[Any, Throwable, Unit] =
     for {
-      _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem:")
-      path <- Console.readLine
-      _ <-  Console.printLine(s"You entered: $path")
-
+      path <- promptForFilePath
       array <- JSONReader.read(path)
       _ <- Console.printLine("The Sudoku problem:")
       _ <- Console.printLine(SudokuSolver.prettyString(array))
-      // Add your Sudoku solver logic here, utilizing ZIO and interacting with the ZIO Console
-
-      // Print the solution
       _ <- Console.printLine("The solution:")
       _ <- ZIO.succeed(SudokuSolver.solve(array))
     } yield ()
-}
 
+  def promptForFilePath: ZIO[Any, Throwable, String] =
+    for {
+      _ <- Console.print(
+        "Enter the path to the JSON file containing the Sudoku problem (hit Enter to use default):"
+      )
+      pathInput <- Console.readLine
+      path = if (pathInput.isEmpty) defaultPath else pathInput
+      fileExistsEffect <- ZIO
+        .attempt(java.nio.file.Files.exists(java.nio.file.Paths.get(path)))
+        .either
+      result <- fileExistsEffect match {
+        case Right(true) => ZIO.succeed(path)
+        case Right(false) =>
+          Console.printLine(
+            "File does not exist, please try again."
+          ) *> promptForFilePath
+        case Left(ex) =>
+          Console.printLine(
+            s"An error occurred: ${ex.getMessage}"
+          ) *> promptForFilePath
+      }
+    } yield result
+
+}
